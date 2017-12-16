fc7f026a24164ce1a0ccac32f91b2d9551b8f91c
basfor1 <800-99@mail.ru>
2017-12-17 00:58:27 +0300

    Fixed indents, file names

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -2,63 +2,63 @@
 using UnityEngine;
 
 public class FileManager
+{
+    private static FileManager instance;
+    public static FileManager GetInstance()
     {
-        private static FileManager instance;
-        public static FileManager GetInstance()
-        {
-            if (instance == null)
-                instance = new FileManager();
-            return instance;
-        }
+        if (instance == null)
+            instance = new FileManager();
+        return instance;
+    }
 
-        private FileManager()
-        {
-        }
+    private FileManager()
+    {
+    }
 
-        public void RewriteIntInFile(int value, string filePath)
-        {
-            CreateNewFile(filePath);
-            //FileStream file = File.OpenWrite(filePath);
+    public void RewriteIntInFile(int value, string filePath)
+    {
+        CreateNewFile(filePath);
+        //FileStream file = File.OpenWrite(filePath);
         //TextWriter writer = new StreamWriter(Stream.Synchronized(file));
         //TextWriter writer = new StreamWriter(filePath);
         File.WriteAllLines(filePath, new string[1] { value.ToString() });
         /*writer.WriteLine(value);
             writer.Close();
             file.Close();*/
-        }
+    }
 
-        public int? ReadFromFile(string filePath)
-        {
-           // FileStream file = File.OpenRead(filePath);
+    public int? ReadFromFile(string filePath)
+    {
+        // FileStream file = File.OpenRead(filePath);
         //TextReader reader = new StreamReader(Stream.Synchronized(file));
         //TextReader reader = new StreamReader(filePath);
         string[] res = File.ReadAllLines(filePath);
-            int? result = null;
-            try
-            {
-                result = int.Parse(res[0]);
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-                //reader.Close();
-                //file.Close();
-            }
-            return result;
-        }
-
-        public void CreateIfNotExist(string filePath)
+        int? result = null;
+        try
         {
-            FileStream file = File.Open(filePath, FileMode.OpenOrCreate);
-            file.Close();
+            result = int.Parse(res[0]);
         }
-
-        private void CreateNewFile(string filePath)
+        catch
         {
-            FileStream file = File.Open(filePath, FileMode.Create);
-            file.Close();
+
         }
+        finally
+        {
+            //reader.Close();
+            //file.Close();
+        }
+        return result;
     }
+
+    public void CreateIfNotExist(string filePath)
+    {
+        FileStream file = File.Open(filePath, FileMode.OpenOrCreate);
+        file.Close();
+    }
+
+    private void CreateNewFile(string filePath)
+    {
+        FileStream file = File.Open(filePath, FileMode.Create);
+        file.Close();
+    }
+}
